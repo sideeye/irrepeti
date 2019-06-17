5681b17032fc7b58791fe2326b22cd96b2034297
Alex Ch <t2alexv@yandex.ru>
2019-06-17 14:14:51 +0300

    Simple auth templates with django auth backend.

--- irrepeti/urls.py
+++ irrepeti/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('dictionary.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')), 
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('frontend.urls')),
+    
 ]
