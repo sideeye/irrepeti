960e1bdf00d5fdccfb5f3f54afc3e59e994a09af
Alex Ch <t2alexv@yandex.ru>
2019-06-18 20:06:46 +0300

    Add unique constraints.

--- dictionary/models.py
+++ dictionary/models.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 class Text(models.Model):
     user = models.ForeignKey(User, related_name='texts', on_delete=models.CASCADE, null=True)
-    text = models.CharField(max_length = 200, unique=True)
+    text = models.CharField(max_length = 200)
     date = models.DateField(auto_now=True)
 
+    class Meta:
+        unique_together = ('user', 'text',)
+    
     def __str__(self):
         return self.text
 
@@ -14,6 +17,9 @@ class Text(models.Model):
 class Translation(models.Model):
     original = models.ForeignKey('Text', related_name='translations', on_delete=models.CASCADE)
     translation = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ('original', 'translation',)
 
     def __str__(self):
         return self.translation
